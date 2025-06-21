05453518fa17527ae0d3e624badd50b52588aca9
DHARSHINI GANAPATHI <dharshiniganapathi76@gmail.com>
2025-06-21 11:24:41 +0530

    Create install_chrome.sh

---
+++ install_chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download and install Chrome
+apt-get update
+apt-get install -y wget unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
